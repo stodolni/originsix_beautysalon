793521f242413667b08a0032f8c6cc4c9296c7bb
Mateus Stodolni <84804882+stodolni@users.noreply.github.com>
2021-07-12 15:12:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ const swiper = new Swiper('.swiper-container', {
 const scrollReveal = ScrollReveal({
   origin:'top',
   distance: '30px',
-  durantion: 700,
+  duration: 700,
   reset: true
 })
 
